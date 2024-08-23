18a05a56bc6dfed75af995f4d8cb5cbb68a586a6
ches-cake <126897112+ches-cake@users.noreply.github.com>
2024-08-23 15:03:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let average = sum / bufferLength;
   
          if (window.innerWidth <= 768) {
-        return average > 30;
+        return average > 40;
     } else {
         return average > 125;
     }
